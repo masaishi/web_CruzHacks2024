e2484ec8a85ad8e82c45d7374c13875a1b89d6f9
shiblumi <diwilson@ucsc.edu>
2024-01-20 11:14:06 -0800

    Colored chips and added flexwrap behavior.

--- src/pages/temp.js
+++ src/pages/temp.js
@@ -1,44 +1,18 @@
+import ColoredChips from '../components/chips';
 import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
-import Chip from '@mui/material/Chip';
-import classes from '../styles/temp.module.css';
-
-const samples = [
-  'One',
-  'Two',
-  'Three',
-  'Four',
-  'Five',
-  'Six',
-  'Seven',
-  'Eight',
-  'Nine',
-  'Ten',
-];
-
-// TODO create variable for pos/neut/neg to color. Create variables
-// in general.
-function createColoredChips(keywords) {
-  return keywords.map((keyword) => {
-    return <Chip label={keyword} variant='outlined' color='success' clickable/>;
-  });
-}
-
-// TODO flexwrap in Box and space out.
-export default function Test({ res }) {
+export default function temp() {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-      <div className='main'>
-        <h1>Temp</h1>
-        {createColoredChips(samples)}
-      </div>
+    <Box display='flex' justifyContent='center' alignContent='space'>
+      <Box border='solid' borderColor='red' width='30%'>
+        <ColoredChips />
+      </Box>
+      <Box border='solid' borderColor='blue' width='30%'>
+        <ColoredChips />
+      </Box>
+      <Box border='solid' borderColor='green' width='30%'>
+        <ColoredChips />
+      </Box>
     </Box>
   );
 }
