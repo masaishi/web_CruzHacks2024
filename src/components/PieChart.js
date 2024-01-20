a21fb83c81a2b5e609e6e081b9e102d4bd66e000
shoot649854 <jojoto9945@icloud.com>
2024-01-20 13:23:26 -0800

    🧹 Cleaning up codes for readability and speed

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,27 +1,19 @@
 import React from "react";
 import { Doughnut } from "react-chartjs-2";
 import { Chart, ArcElement } from "chart.js";
-import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+import { isMobile } from "react-device-detect";
 
 Chart.register(ArcElement);
 
 const data = {
   labels: ["Negative", "Neutral", "Positive"],
   datasets: [
-    {
-      data: [30, 20, 50],
-      backgroundColor: ["#FFB3B3", "#FFD699", "#B3D9FF"],
-    },
+    { data: [30, 20, 50], backgroundColor: ["#FFB3B3", "#FFD699", "#B3D9FF"] },
   ],
 };
 
 const colorize = (opaque, hover, ctx) => {
   const v = ctx.parsed;
-  const c =
-    v < -50 ? "#D60000" : v < 0 ? "#F46300" : v < 50 ? "#0358B6" : "#44DE28";
-
-  const opacity = hover ? 1 - Math.abs(v / 150) - 0.2 : 1 - Math.abs(v / 150);
-
   return opaque;
 };
 
@@ -30,11 +22,8 @@ const hoverColorize = (ctx) => {
 };
 
 const options = {
-  plugins: {
-    legend: true,
-    tooltip: false,
-  },
-  responsive: true, 
+  plugins: { legend: true, tooltip: false },
+  responsive: true,
   maintainAspectRatio: !isMobile,
   elements: {
     arc: {
@@ -46,7 +35,7 @@ const options = {
 
 const PieChart = () => {
   return (
-    <div style={{ width: isMobile ? '75%' : '100%' }}>
+    <div style={{ width: isMobile ? "75%" : "100%" }}>
       <Doughnut data={data} options={options} />
     </div>
   );
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,22 +1,14 @@
-'use client';
-import * as React from 'react';
-import {
-  Grid,
-  Avatar,
-  CssBaseline,
-  Link,
-  Box,
-  Typography,
-  Container,
-} from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
+"use client";
+import * as React from "react";
+import { Avatar, CssBaseline, Box, Typography, Container } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
-import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+import { isMobile } from "react-device-detect";
 
-import GoogleSearchBar from '@/components/GoogleSearchBar';
-import ColoredChips from '@/components/chips';
-import PieChart from '@/components/PieChart';
-import Copyright from '@/components/Copyright';
+import GoogleSearchBar from "@/components/GoogleSearchBar";
+import ColoredChips from "@/components/chips";
+import PieChart from "@/components/PieChart";
+import Copyright from "@/components/Copyright";
 
 // const defaultTheme = createTheme();
 
@@ -25,41 +17,40 @@ function Dashboard() {
     <Container>
       <CssBaseline />
       <Box
-        width={'100%'}
+        width={"100%"}
         sx={{
           marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          textAlign: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
         }}
       >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
           <SearchIcon />
         </Avatar>
-        <Typography component='h1' variant='h5'>
+        <Typography component="h1" variant="h5">
           Cruz Hack 2024
         </Typography>
         <Box
-          width={isMobile ? '70%' : '130%'}
-          // height={'75%'}
+          width={isMobile ? "70%" : "130%"}
           marginTop={5}
           className="flex-container"
           sx={{
-            display: 'flex',
-            justifyContent: 'space-around',
-            gap: '15%',
+            display: "flex",
+            justifyContent: "space-around",
+            gap: "15%",
           }}
         >
           {/* GoogleSearchBar */}
           <Box
             marginTop={5}
-            width={'100%'}
-            height={'75%'}
+            width={"100%"}
+            height={"75%"}
             className="component"
             sx={{
-              flexDirection: isMobile ? 'row' : 'column',
+              flexDirection: isMobile ? "row" : "column",
             }}
           >
             <GoogleSearchBar />
@@ -67,20 +58,16 @@ function Dashboard() {
           </Box>
 
           {/* Section */}
-          <Box
-            width={'100%'}
-            height={'75%'}
-            className="component"
-          >
+          <Box width={"100%"} height={"75%"} className="component">
             <Typography>Section</Typography>
           </Box>
 
           {/* PieChart */}
           <Box
-            width={isMobile ? '120%' : '100%'}
+            width={isMobile ? "120%" : "100%"}
             className="component"
             sx={{
-              flexDirection: isMobile ? 'row' : 'column',
+              flexDirection: isMobile ? "row" : "column",
             }}
           >
             <PieChart />
@@ -89,12 +76,12 @@ function Dashboard() {
       </Box>
       <Box
         sx={{
-          position: 'absolute',
+          position: "absolute",
           bottom: 0,
           left: 0,
-          width: '100%',
-          textAlign: 'center',
-          padding: '1rem',
+          width: "100%",
+          textAlign: "center",
+          padding: "1rem",
         }}
       >
         <Copyright />
