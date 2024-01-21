119a512a94c4ecb8818cc73fbc277a994eb484c3
shoot649854 <jojoto9945@icloud.com>
2024-01-20 19:54:38 -0800

    ✨ Adding key prop for element in iterator

--- src/components/PostColumn.js
+++ src/components/PostColumn.js
@@ -3,12 +3,14 @@ import Post from '@/components/Post';
 import dummy_data from '@/temp/message.json';
 
 export default function PostColumn(props) {
-  const posts = dummy_data.map((post) => {
-    return <Post
-              post={post['sentences']}
-              clickedContentDashboard={props.clickedContentDashboard}
-            />
-  });
+  const posts = dummy_data.map((post, index) => (
+    <Post
+      key={index}
+      post={post['sentences']}
+      clickedContentDashboard={props.clickedContentDashboard}
+    />
+  ));
+
 
   return <Box>{posts}</Box>;
 }
